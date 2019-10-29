2813adf9b420e60151495c1ba0096995fa3d5c3b
Linnaea <oddradishthe1st@gmail.com>
2019-10-28 20:13:06 -0700

    Added OpMode files

---
+++ TeamCode_12110Titans/src/main/java/org/firstinspires/ftc/teamcode_12110titans/DS1OpMode.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode_12110titans;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class DS1OpMode extends LinearOpMode {
+}
---
+++ TeamCode_12110Titans/src/main/java/org/firstinspires/ftc/teamcode_12110titans/Startpos2B.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode_12110titans;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Startpos2B extends LinearOpMode {
+}
