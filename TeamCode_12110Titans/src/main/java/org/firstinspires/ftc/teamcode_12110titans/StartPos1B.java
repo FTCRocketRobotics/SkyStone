c8746921994a3ffeb042e429de4c85318dde4421
Vibhav <vibhav.javali@gmail.com>
2019-10-29 18:52:19 -0700

    Change to All OpModes
    change to PickleOpMode

--- TeamCode_12110Titans/src/main/java/org/firstinspires/ftc/teamcode_12110titans/StartPos1B.java
+++ TeamCode_12110Titans/src/main/java/org/firstinspires/ftc/teamcode_12110titans/StartPos1B.java
@@ -2,5 +2,5 @@ package org.firstinspires.ftc.teamcode_12110titans;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class StartPos1B extends LinearOpMode {
-}
+//public class StartPos1B extends LinearOpMode {
+//}
