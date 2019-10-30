fbeb9394c73eb97180d49ade3cbe03b0385ffa3b
Vibhav <vibhav.javali@gmail.com>
2019-10-29 18:52:19 -0700

    Change to PickleOpMode
    change to PickleOpMode

--- TeamCode_12110Titans/src/main/java/org/firstinspires/ftc/teamcode_12110titans/DS1OpMode.java
+++ TeamCode_12110Titans/src/main/java/org/firstinspires/ftc/teamcode_12110titans/DS1OpMode.java
@@ -2,5 +2,5 @@ package org.firstinspires.ftc.teamcode_12110titans;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class DS1OpMode extends LinearOpMode {
-}
+//public class DS1OpMode extends LinearOpMode {
+
