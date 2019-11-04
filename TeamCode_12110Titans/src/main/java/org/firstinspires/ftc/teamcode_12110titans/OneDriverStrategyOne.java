efa8f67daf96a249394385b11711c5058dba0995
Linnaea <oddradishthe1st@gmail.com>
2019-11-03 21:29:35 -0800

    Made a New TeleOp Mode for mecanum Wheels

---
+++ TeamCode_12110Titans/src/main/java/org/firstinspires/ftc/teamcode_12110titans/OneDriverStrategyOne.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode_12110titans;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
+
+@TeleOp
+public class OneDriverStrategyOne extends LinearOpMode {
+
+    //We love Linnaea's OpModes
+    private DcMotor fL;
+    private DcMotor fR;
+    private DcMotor bL;
+    private DcMotor bR;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
+
+        bL = hardwareMap.get(DcMotor.class, "bL");
+        bR = hardwareMap.get(DcMotor.class, "bR");
+        fL = hardwareMap.get(DcMotor.class, "fL");
+        fR = hardwareMap.get(DcMotor.class, "fR");
+
+        bL.setDirection(DcMotor.Direction.FORWARD);
+        bR.setDirection(DcMotor.Direction.REVERSE);
+        fL.setDirection(DcMotor.Direction.FORWARD);
+        fR.setDirection(DcMotor.Direction.REVERSE);
+
+        //Wait for driver to press PLAY
+        waitForStart();
+
+        //run until the driver presses STOP
+        while(opModeIsActive()){
+
+            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_y;
+
+            double direction_travel;
+            double direction_wheels;
+
+            double lF_power;
+            double rF_power;
+            double lB_power;
+            double rB_power;
+
+            direction_travel=Math.atan2(y,x);
+            direction_wheels=direction_travel-Math.PI/4;
+
+            lF_power=Math.cos(direction_wheels);
+            rF_power=Math.sin(direction_wheels);
+            lB_power=Math.sin(direction_wheels);
+            rB_power=Math.cos(direction_wheels);
+
+            fL.setPower(lF_power);
+            fR.setPower(rF_power);
+            bL.setPower(lB_power);
+            bR.setPower(rB_power);
+
+            telemetry.addData("Status","Running");
+            telemetry.update();
+        }
+
+
+
+
+    }
+
+
+}
+
